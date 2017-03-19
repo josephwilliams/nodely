b204f3cb32d917c3372e486ffa684ac258874f4d
josephwilliams <josephwilli12@gmail.com>
2017-03-19 13:40:12 -0700

    added husky precommit and prepush; runs yarn test before git commit or push

--- src/dog.js
+++ src/dog.js
@@ -1,10 +1,10 @@
 class Dog {
   constructor(name) {
-    this.name = name
+    this.name = name;
   }
 
   bark() {
-    return `Wah wah, I am ${this.name}`
+    return `Wah wah, I am ${this.name}`;
   }
 }
 
